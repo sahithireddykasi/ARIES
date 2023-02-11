97a688a7d7a20d589d27894f855bb9bfaf00d2b4
sahithireddykasi <117863480+sahithireddykasi@users.noreply.github.com>
2023-02-11 14:35:10 -0500

    Add files via upload

---
+++ array.java
@@ -0,0 +1,21 @@
+package sahithipractice;
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] s = {"10","20"};
+		for(int i=0;i<s.length;i++)
+		{
+		
+		System.out.println(s[i]);
+		}
+		int x = Integer.parseInt(s[0]);
+		int y = Integer.parseInt(s[1]);
+		int z = x+y;
+		System.out.println(z);
+		
+
+	}
+
+}
